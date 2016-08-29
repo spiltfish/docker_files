6194eb0cb5d3a7418767efe6a177c083b7e41ada
Mary Larrousse <mary.larrousse@hds.com>
2016-08-29 14:19:06 -0700

    adding commands to generate new dockerfiles for new FTB packs

---
+++ minecraft/generate_docker_file.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ ! $# -eq 2 ]
+   then
+    echo "Usage: ./generate_docker_file.sh <server zip url> <server tag>"
+    exit 1
+fi
+
+#Parameter Definition
+SERVER_ZIP_URL=$1
+SERVER_TAG=$2
+
+if [ ! -d "$SERVER_TAG" ]; then
+    mkdir $SERVER_TAG
+fi
+
+echo 'Createing Dockerfile for server '$SERVER_ZIP_URL
+
+sed -e 's/$SERVER_ZIP_URL/'$SERVER_ZIP_URL'/g' FTB_Dockerfile.tmpl > $SERVER_TAG/Dockerfile
+
---
+++ minecraft/generate_run_command.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [! $# -eq 1 ]
+   then
+    echo "Usage: ./generate_run_command.sh <server tag>"
+    exit 1
+fi
+
+#Parameter Definition
+SERVER_TAG=$1
+
+if [ ! -d "$SERVER_TAG" ]; then
+    mkdir $SERVER_TAG
+fi
+
+touch $SERVER_TAG/docker_run.sh
+
+sed -e 's/$SERVER_TAG/'$SERVER_TAG'/g' docker_run.sh.tmpl > $SERVER_TAG/docker_run.sh
